1778786468b9024c63f145a9df0da91934fd30a5
Seif <seifeddine.zaddem@external.grtgaz.com>
2022-11-01 23:21:59 +0100

    scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AbcComponent } from './abc/abc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbcComponent
   ],
   imports: [
     BrowserModule
